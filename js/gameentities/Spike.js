fdc0a5d7495be3c2f43456e4c78670b624c2af35
Joshua Pena <penacalata@gmail.com>
2014-11-15 00:12:39 -0800

    added beginning files to coreProject

---
+++ js/gameentities/Spike.js
@@ -0,0 +1,32 @@
+"use strict";
+
+var Spike = function(world, options) {
+    this.world = world;
+
+    this.spriteName = null;
+
+    this.width = 30;
+    this.height = 30;
+    this.x = this.world.width / 2;
+    this.y = this.world.height - this.height;
+    this.type = "enemy";
+
+    this.hitbox = {
+        x: this.x,
+        y: this.y,
+        width: 10,
+        height: 10
+    };
+};
+
+Spike.prototype.draw = function() {
+    if (this.spriteName === null) {
+        this.world.drawRectangle("#008000", this.x, this.y, this.width, this.height);
+    } else {
+        this.world.drawSprite(this.spriteName, this.x, this.y, this.width, this.height);
+    }
+
+};
+
+Spike.prototype.explode = function() {
+};
---
+++ js/lib/update.js
@@ -0,0 +1,124 @@
+"use strict";
+
+// Stages
+var nextStage = false;
+var minionTime = false;
+var bossTime = true;
+var round = 0;
+
+var test = true;
+
+var update = function (game, CatEnemy, Bullet, audio, Explosion) {
+
+	[game.world.platforms,
+	].forEach (
+		function(gameElementArray) {
+			gameElementArray.forEach(function(gameElement) {
+				gameElement.update();
+			});
+		}
+	);
+	
+	game.world.players.forEach( function(player) {
+        player.update();
+    });
+
+    game.world.players = game.world.players.filter(function(player) {
+        return player.active;
+    });
+	
+	game.world.platforms = game.world.platforms.filter(function(platform) {
+		return platform.active;
+	});
+	
+	if (game.world.players.length < 1) {
+       game.world.died = true; 
+    } else if (test) {
+        game.world.spikes.push (new Spike(game.world));
+        game.world.platforms.push (new Platform(game.world, {
+            x: 0,
+            y: game.world.height - 30,
+            width: game.world.width,
+            height: 200
+        }));
+
+        game.world.platforms.push(new Platform(game.world, {
+			x: 175,
+			y: 250,
+		}));
+		game.world.platforms.push(new Platform(game.world, {
+			x: 375,
+			y: 250,
+		}));
+		game.world.platforms.push(new Platform(game.world, {
+			x: 130,
+			y: 196,
+		}));
+		game.world.platforms.push(new Platform(game.world, {
+			x: 420,
+			y: 196,
+		}));
+        game.world.platforms.push(new Platform(game.world, {
+            x: 130,
+            y: 140
+        }));
+        game.world.platforms.push(new Platform(game.world, {
+            x: 420,
+            y: 140
+        }));
+        game.world.platforms.push(new Platform(game.world, {
+            x: 175,
+            y: 83
+        }));
+        game.world.platforms.push(new Platform(game.world, {
+            x: 375,
+            y: 83
+        }));
+        game.world.platforms.push(new Platform(game.world, {
+            x: 275,
+            y: 83
+        }));
+
+        test = false;
+	} else if (minionTime && round == 0) {
+        game.world.platforms.push(new Platform(game.world, {
+			x: 175,
+			y: 250,
+		}));
+		game.world.platforms.push(new Platform(game.world, {
+			x: 375,
+			y: 250,
+		}));
+		game.world.platforms.push(new Platform(game.world, {
+			x: 130,
+			y: 196,
+		}));
+		game.world.platforms.push(new Platform(game.world, {
+			x: 420,
+			y: 196,
+		}));
+        game.world.platforms.push(new Platform(game.world, {
+            x: 130,
+            y: 140
+        }));
+        game.world.platforms.push(new Platform(game.world, {
+            x: 420,
+            y: 140
+        }));
+        game.world.platforms.push(new Platform(game.world, {
+            x: 175,
+            y: 83
+        }));
+        game.world.platforms.push(new Platform(game.world, {
+            x: 375,
+            y: 83
+        }));
+        game.world.platforms.push(new Platform(game.world, {
+            x: 275,
+            y: 83
+        }));
+
+    } else if (minionTime && round == 5) {
+		game.world.end = true;	
+	}
+};
